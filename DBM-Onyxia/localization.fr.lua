70f808be1072d03148b1a182a6257a122c603069
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2020-07-07 00:37:43 +0000

    Update localization.fr.lua

--- DBM-Onyxia/localization.fr.lua
+++ DBM-Onyxia/localization.fr.lua
@@ -28,7 +28,7 @@ L:SetOptionLocalization{
 L:SetMiscLocalization{
 	Breath = "prend une grande inspiration",
 	YellPull = "Quelle chance ! D'habitude, je dois quitter mon repaire pour me nourrir."
-	YellP2 = "Cet exercice dénué de sens m'ennuie. Je vais vous incinérer d'un seul coup !",
+	YellP2 = "Cet exercice dénué de sens m'ennuie. Je vais vous incinérer d'un seul coup !"
 	YellP3 = "Il semble que vous ayez besoin d'une autre leçon, mortels !"
 }
 
