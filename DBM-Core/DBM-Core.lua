ca8557646d98281ebdc29f59b1589d6c5a725491
MysticalOS <mysticalosx@gmail.com>
2021-03-09 14:02:46 -0500

    Also fixed a bug where AddMsg would fail to read chatframe setting when AddMsg is called by a mod and not core. Self would inherit the wrong parent

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -7255,7 +7255,7 @@ end
 -----------------------
 function DBM:AddMsg(text, prefix)
 	local tag = prefix or (self.localization and self.localization.general.name) or L.DBM
-	local frame = self.Options.ChatFrame and _G[tostring(self.Options.ChatFrame)] or DEFAULT_CHAT_FRAME
+	local frame = DBM.Options.ChatFrame and _G[tostring(DBM.Options.ChatFrame)] or DEFAULT_CHAT_FRAME
 	frame = frame and frame:IsShown() and frame or DEFAULT_CHAT_FRAME
 	if prefix ~= false then
 		frame:AddMessage(("|cffff7d0a<|r|cffffd200%s|r|cffff7d0a>|r %s"):format(tostring(tag), tostring(text)), 0.41, 0.8, 0.94)
